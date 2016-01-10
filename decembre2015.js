fcc4cd92c89b963045c02575d6d8f219d6538f68
Renaud Hoyoux <renaud.hoyoux@yahoo.fr>
2016-01-10 15:44:29 +0100

    2eme exercice : Compteur de clics

--- decembre2015.js
+++ decembre2015.js
@@ -1,5 +1,12 @@
 <script>
-var message = "Hello world !";
-alert(message);
-</script>
+// ceci est un commentaire, il ne sera pas lu par l'ordinateur
 
+var compteur = 0;
+
+function clique() {
+	// la ligne suivante signifie que mon compteur est augmenté de 1
+	compteur = compteur+1;
+	alert(compteur);
+}
+</script>
+<button onclick="clique()">Ajouter</button>
