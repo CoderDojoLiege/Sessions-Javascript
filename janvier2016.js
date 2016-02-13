14b69f3a6b223b24a0d73c71ef22fb7db4eeb5a6
Renaud Hoyoux <renaud.hoyoux@yahoo.fr>
2016-02-13 17:26:00 +0100

    fix PV bug

--- janvier2016.js
+++ janvier2016.js
@@ -6,21 +6,23 @@ var degats = 3;
 var soins = 2;
 
 function attaque() {
-    PV = PV-degats;
-    if(PV <= 0){
+    if(PV-degats <= 0){ //cela veut dire que le monstre sera mort après l attaque.
+        PV = 0; // si il est mort, on ne veut pas de PV dans le négatifs. On mets donc les PV à 0.
         alert("Le monstre est mort ! :-)");
     }
     else {
+        PV = PV-degats;
         alert("Le monstre a perdu "+degats+" PV. Il lui en reste "+PV);
     }
 }
 
 function soigne() {
-    PV = PV+soins;
-    if(PV >= 12){
+    if(PV+soins >= 12){ //cela veut dire que le monstre sera en pleine santé après le soin.
+        PV = 12; // On mets donc les PV au maximum.
         alert("Le monstre a ses points de vie déjà au maximum.");
     }
     else {
+        PV = PV+soins;
         alert("Le monstre a gagné "+soins+" PV. Il en a maintenant "+PV);
     }
 }
